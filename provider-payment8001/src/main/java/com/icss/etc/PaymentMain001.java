646cb099c7cd1c63e44ff118c5a3babfcf1ae312
wutianhang <mr_wu163@163.com>
2023-09-27 14:39:56 +0800

    提交

---
+++ provider-payment8001/src/main/java/com/icss/etc/PaymentMain001.java
@@ -0,0 +1,13 @@
+package com.icss.etc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMain001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain001.class, args);
+        System.out.println("启动成功");
+    }
+}
